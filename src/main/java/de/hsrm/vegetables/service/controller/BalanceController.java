325dbd339fbd56b1cf5c1f8dd4a80073f9123c91
Manuel Serret <manuel-serret@t-online.de>
2020-12-09 11:05:43 +0100

    [VEG-30]: FEAT: Load and save balance from Frontend

---
+++ src/main/java/de/hsrm/vegetables/service/controller/BalanceController.java
@@ -0,0 +1,45 @@
+package de.hsrm.vegetables.service.controller;
+
+import de.hsrm.vegetables.Stadtgemuese_Backend.api.BalanceApi;
+import de.hsrm.vegetables.Stadtgemuese_Backend.api.HelloWorldApi;
+import de.hsrm.vegetables.Stadtgemuese_Backend.model.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/v1")
+@CrossOrigin(origins = "*")
+public class BalanceController implements BalanceApi {
+
+    private final HashMap<String, BigDecimal> balances;
+
+    public BalanceController() {
+        this.balances = new HashMap<>();
+    }
+
+    @Override
+    public ResponseEntity<BalanceGetResponse> balanceGet(String name) {
+        BalanceGetResponse response = new BalanceGetResponse();
+
+        if (balances.containsKey(name)) {
+            response.balance(balances.get(name));
+        } else {
+            response.balance(new BigDecimal(0));
+        }
+
+        return ResponseEntity.ok(response);
+    }
+
+    @Override
+    public ResponseEntity<Void> balancePost(String name, BalancePostRequest request) {
+        balances.put(name, request.getBalance());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
