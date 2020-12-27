5fce3cb5f26fe527df3c688a4aa19c0dfe171f61
Vincent Bäertsch <vincent.baertsch@student.hs-rm.de>
2020-12-27 19:10:35 +0100

    [VEG-79] feat: Add stock to database and get stock by id

--- src/main/java/de/hsrm/vegetables/service/mapper/StockMapper.java
+++ src/main/java/de/hsrm/vegetables/service/mapper/StockMapper.java
@@ -1,6 +1,7 @@
 package de.hsrm.vegetables.service.mapper;
 
 import de.hsrm.vegetables.Stadtgemuese_Backend.model.StockResponse;
+import de.hsrm.vegetables.Stadtgemuese_Backend.model.StockResponseById;
 import de.hsrm.vegetables.service.domain.dto.StockDto;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,6 +9,16 @@ import java.util.stream.Collectors;
 public class StockMapper {
     public static StockResponse stockDtoToStockResponse (StockDto stockDto) {
         StockResponse response = new StockResponse();
+        response.id(stockDto.getId());
+        response.name(stockDto.getName());
+        response.unitType(stockDto.getUnitType());
+        response.quantity(stockDto.getQuantity());
+        response.description(stockDto.getDescription());
+        return response;
+    }
+
+    public static StockResponseById stockResponseById (StockDto stockDto) {
+        StockResponseById response = new StockResponseById();
         response.id(stockDto.getId());
         response.name(stockDto.getName());
         response.unitType(stockDto.getUnitType());
