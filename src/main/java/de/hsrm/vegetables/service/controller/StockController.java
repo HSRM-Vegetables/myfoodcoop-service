47ce5c7b49770e83ac10928109bd99165ba604f2
Vincent Bäertsch <vincent.baertsch@student.hs-rm.de>
2020-12-27 17:12:37 +0100

    [VEG-79] feat: Create Database and add findall function

--- src/main/java/de/hsrm/vegetables/service/controller/StockController.java
+++ src/main/java/de/hsrm/vegetables/service/controller/StockController.java
@@ -2,14 +2,26 @@ package de.hsrm.vegetables.service.controller;
 
 import de.hsrm.vegetables.Stadtgemuese_Backend.api.StockApi;
 import de.hsrm.vegetables.Stadtgemuese_Backend.model.*;
+import de.hsrm.vegetables.service.domain.dto.StockDto;
+import de.hsrm.vegetables.service.mapper.StockMapper;
+import de.hsrm.vegetables.service.services.StockService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/v1")
 @CrossOrigin(origins = "*")
+@RequiredArgsConstructor(onConstructor = @__({@Autowired}))
 public class StockController implements StockApi {
+
+    @NonNull
+    private final StockService stockService;
 
     @Override
     public ResponseEntity<Void> stockDelete(String itemId) {
@@ -18,7 +30,10 @@ public class StockController implements StockApi {
 
     @Override
     public ResponseEntity<AllStockResponse> stockGet() {
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+        List<StockResponse> items = StockMapper.listStockDtoToListStockResponse(stockService.getAll());
+        AllStockResponse response = new AllStockResponse();
+        response.setItems(items);
+        return ResponseEntity.ok(response);
     }
 
     @Override
---
+++ src/main/java/de/hsrm/vegetables/service/domain/dto/StockDto.java
@@ -0,0 +1,37 @@
+package de.hsrm.vegetables.service.domain.dto;
+
+import de.hsrm.vegetables.Stadtgemuese_Backend.model.UnitType;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity // Persists this class
+@EntityListeners(AuditingEntityListener.class)
+@Data
+public class StockDto {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private UnitType unitType;
+
+    @Column(nullable = false)
+    private Float quantity;
+
+    @Column(nullable = false)
+    private Float pricePerUnit;
+
+    @Column(length = 10000)
+    private String description;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+
+}
