d90fd12cd5ff62cbc6c992e6a48d799622a894d4
Vincent Bäertsch <vincent.baertsch@student.hs-rm.de>
2020-12-27 17:12:37 +0100

    [VEG-79] feat: Create Database and add findall function

---
+++ src/main/java/de/hsrm/vegetables/service/mapper/StockMapper.java
@@ -0,0 +1,25 @@
+package de.hsrm.vegetables.service.mapper;
+
+import de.hsrm.vegetables.Stadtgemuese_Backend.model.StockResponse;
+import de.hsrm.vegetables.service.domain.dto.StockDto;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StockMapper {
+    public static StockResponse stockDtoToStockResponse (StockDto stockDto) {
+        StockResponse response = new StockResponse();
+        response.id(stockDto.getId());
+        response.name(stockDto.getName());
+        response.unitType(stockDto.getUnitType());
+        response.quantity(stockDto.getQuantity());
+        response.description(stockDto.getDescription());
+        return response;
+    }
+
+    public static List<StockResponse> listStockDtoToListStockResponse (List<StockDto> stockDtos) {
+        return stockDtos
+                .stream()
+                .map(stockDto -> stockDtoToStockResponse(stockDto))
+                .collect(Collectors.toList());
+    }
+}
