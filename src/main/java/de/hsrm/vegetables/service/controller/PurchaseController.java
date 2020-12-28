01056594daae6d8481ebb34930c044d824f5d1e0
Marvin Kaiser <marvin.kaiser.93@gmail.com>
2020-12-28 01:58:56 +0100

    [VEG-79] feat: implement purchase

--- src/main/java/de/hsrm/vegetables/service/controller/PurchaseController.java
+++ src/main/java/de/hsrm/vegetables/service/controller/PurchaseController.java
@@ -3,18 +3,37 @@ package de.hsrm.vegetables.service.controller;
 import de.hsrm.vegetables.Stadtgemuese_Backend.api.PurchaseApi;
 import de.hsrm.vegetables.Stadtgemuese_Backend.model.PurchaseRequest;
 import de.hsrm.vegetables.Stadtgemuese_Backend.model.PurchaseResponse;
-import org.springframework.http.HttpStatus;
+import de.hsrm.vegetables.service.services.StockService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1")
 @CrossOrigin(origins = "*")
+@RequiredArgsConstructor(onConstructor = @__({@Autowired}))
 public class PurchaseController implements PurchaseApi {
+
+    @NonNull
+    private final StockService stockService;
 
     @Override
     public ResponseEntity<PurchaseResponse> purchaseFromStock(String name, PurchaseRequest purchaseRequest) {
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+
+        // TODO: Check that user exists before next call
+
+        Float totalPrice = stockService.purchase(purchaseRequest.getItems());
+
+        // TODO: add balance handling
+
+        PurchaseResponse response = new PurchaseResponse();
+        response.setName(name);
+        response.setPrice(totalPrice);
+        response.setBalance(0f); // TODO: add balance handling
+
+        return ResponseEntity.ok(response);
     }
 
 }
