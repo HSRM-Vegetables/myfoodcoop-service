0af8a6e9ae2385045119c306baa38ccc89cd6f94
Marvin Kaiser <marvin.kaiser.93@gmail.com>
2020-12-28 01:58:56 +0100

    [VEG-79] feat: implement purchase

--- src/test/java/karate/purchase/PurchaseTest.java
+++ src/test/java/karate/purchase/PurchaseTest.java
@@ -2,11 +2,17 @@ package karate.purchase;
 
 import com.intuit.karate.junit5.Karate;
 import de.hsrm.vegetables.service.Application;
+import de.hsrm.vegetables.service.domain.dto.StockDto;
+import de.hsrm.vegetables.service.repositories.StockRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
 
 @SpringBootTest(
         webEnvironment = WebEnvironment.DEFINED_PORT,
@@ -18,7 +24,18 @@ import org.springframework.test.context.TestPropertySource;
         }
 )
 @DirtiesContext
+@AutoConfigureMockMvc
 class PurchaseTest {
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @AfterEach
+    void cleanUp() {
+        List<StockDto> stockList = stockRepository.findAll();
+        stockList.forEach(stock -> stockRepository.delete(stock));
+    }
+
 
     @Karate.Test
     Karate testAll() {
