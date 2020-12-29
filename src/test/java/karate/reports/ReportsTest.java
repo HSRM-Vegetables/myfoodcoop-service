83dd05b725735a0104150d4bedeb67d627051baf
Marvin Kaiser <marvin.kaiser.93@gmail.com>
2020-12-29 14:48:26 +0100

    [VEG-102] test: add tests for report items sold, Lars Keim

---
+++ src/test/java/karate/reports/ReportsTest.java
@@ -0,0 +1,44 @@
+package karate.reports;
+
+import com.intuit.karate.junit5.Karate;
+import de.hsrm.vegetables.service.Application;
+import de.hsrm.vegetables.service.domain.dto.StockDto;
+import de.hsrm.vegetables.service.repositories.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+
+@SpringBootTest(
+        webEnvironment = WebEnvironment.DEFINED_PORT,
+        classes = Application.class
+)
+@TestPropertySource(
+        properties = {
+                "server.port: 8090"
+        }
+)
+@DirtiesContext
+@AutoConfigureMockMvc
+public class ReportsTest {
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @AfterEach
+    void cleanUp() {
+        List<StockDto> stockList = stockRepository.findAll();
+        stockList.forEach(stock -> stockRepository.delete(stock));
+    }
+
+    @Karate.Test
+    Karate testAll() {
+        return Karate.run()
+                .relativeTo(getClass());
+    }
+}
