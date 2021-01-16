7bc4d5fd3ccb48eae092d01133d17d21a6db2b3f
Marvin Kaiser <marvin.kaiser.93@gmail.com>
2021-01-16 17:35:14 +0100

    [VEG-125] tests: move balance tests to v2

--- src/main/java/de/hsrm/vegetables/service/controller/UserController.java
+++ src/main/java/de/hsrm/vegetables/service/controller/UserController.java
@@ -7,6 +7,7 @@ import de.hsrm.vegetables.Stadtgemuese_Backend.model.TokenResponse;
 import de.hsrm.vegetables.Stadtgemuese_Backend.model.UserResponse;
 import de.hsrm.vegetables.service.domain.dto.UserDto;
 import de.hsrm.vegetables.service.mapper.UserMapper;
+import de.hsrm.vegetables.service.services.BalanceService;
 import de.hsrm.vegetables.service.services.UserService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -26,10 +27,16 @@ public class UserController implements UserApi {
     @NonNull
     private final UserService userService;
 
+    @NonNull
+    private final BalanceService balanceService;
+
     @Override
     public ResponseEntity<UserResponse> register(RegisterRequest registerRequest) {
 
         UserDto newUser = userService.register(registerRequest.getUsername(), registerRequest.getEmail(), registerRequest.getMemberId(), registerRequest.getPassword());
+
+        balanceService.upsert(newUser.getUsername(), 0f);
+        
         UserResponse response = UserMapper.userDtoToUserResponse(newUser);
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
