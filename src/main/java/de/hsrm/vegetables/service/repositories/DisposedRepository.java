b0bcc9551de4819459f47fd5b5bcfd0634920779
lkeim001 <larseric.keim@student.hs-rm.de>
2021-02-10 15:14:17 +0100

    [VEG-180] feature: adding DisposeService

--- src/main/java/de/hsrm/vegetables/service/repositories/DisposedRepository.java
+++ src/main/java/de/hsrm/vegetables/service/repositories/DisposedRepository.java
@@ -1,10 +1,17 @@
 package de.hsrm.vegetables.service.repositories;
 
 import de.hsrm.vegetables.service.domain.dto.DisposedDto;
+import de.hsrm.vegetables.service.domain.dto.UserDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.OffsetDateTime;
+import java.util.List;
 
 @Repository
 public interface DisposedRepository extends JpaRepository<DisposedDto, Long> {
 
+    List<DisposedDto> findAllByUserDto(UserDto userDto);
+
+    List<DisposedDto> findAllByCreatedOnBetween(OffsetDateTime fromDate, OffsetDateTime toDate);
 }
